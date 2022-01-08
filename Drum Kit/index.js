a28cc1dd9686b1307ab79547041cdd8de7c88a43
Ninaad <78657749+ninaadps@users.noreply.github.com>
2022-01-08 12:20:25 +0530

    Add files via upload

---
+++ Drum Kit/index.js
@@ -0,0 +1,62 @@
+
+// button press
+var numberOfDrumButtons= document.querySelectorAll(".drum").length;
+
+for(var i=0;i<numberOfDrumButtons;i++)
+{
+document.querySelectorAll(".drum")[i].addEventListener("click",function()
+{
+ var buttonclk= this.innerHTML;
+ makeSound(buttonclk);
+buttonAnimation(buttonclk);
+});
+}
+
+
+
+
+// keyboard press
+document.addEventListener("keydown",function(){
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+
+function makeSound(key)
+{
+  switch(key)
+  {
+    case "w":var audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
+    break;
+    case "a":var audio = new Audio("sounds/tom-2.mp3");
+    audio.play();
+    break;
+    case "s":var audio = new Audio("sounds/tom-3.mp3");
+    audio.play();
+    break;
+    case "d":var audio = new Audio("sounds/tom-4.mp3");
+    audio.play();
+    break;
+    case "j":var audio = new Audio("sounds/snare.mp3");
+    audio.play();
+    break;
+    case "k":var audio = new Audio("sounds/crash.mp3");
+    audio.play();
+    break;
+    case "l":var audio = new Audio("sounds/kick-bass.mp3");
+    audio.play();
+    break;
+
+
+  }
+}
+
+function buttonAnimation(currentKey)
+{
+  var activeButton=document.querySelector("."+currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 50);
+}
